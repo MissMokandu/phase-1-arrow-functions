2d5eff2419d24e542fcd081fac8e03bf5e0add0d
Benadette Mokandu <benadettemokandu@gmail.com>
2025-06-14 01:21:10 +0300

    am getting this

---
+++ index.js
@@ -0,0 +1,19 @@
+function divide(){
+    let a=2000;
+    let b=100;
+    return a/b; 
+}
+console.log (divide());
+
+const square = () => {
+    let x = 2;
+    return x**2;
+}
+console.log(square());
+
+const add = () => {
+    let a = 3;
+    let b = 4;
+    return a + 4;
+}
+console.log(add());
